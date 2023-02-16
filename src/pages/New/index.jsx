3cf93704bf820031c3eff2acb1086612e194a103
Rafael Mendes <faelmob@gmail.com>
2023-02-15 22:27:58 -0300

    iNPLEMENTAÇÃO

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,24 @@
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { TextArea } from "../../components/TextArea";
+
+import { Container, Form } from "./styles";
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar Nota</h1>
+            <a href="/">Voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+          <TextArea placeholder="Observações" />
+        </Form>
+      </main>
+    </Container>
+  );
+}
